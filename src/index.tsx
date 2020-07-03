a0a692a412e8892f138340d572f2823a2b4b10a8
Gaurav Bharti <dreamsparks@outlook.com>
2020-07-04 02:30:02 +0530

    added eslint

--- src/index.tsx
+++ src/index.tsx
@@ -25,10 +25,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// https://medium.com/@pppped/extend-create-react-app-with-airbnbs-eslint-config-prettier-flow-and-react-testing-library-96627e9a9672
-// https://medium.com/@feralamillo/create-react-app-typescript-eslint-and-prettier-699277b0b913
-// https://thomlom.dev/setup-eslint-prettier-react/
-// https://dev.to/botreetechnologies/setting-up-husky-pre-commit-hook-with-eslint-prettier-and-lint-staged-for-react-and-react-native-d05
-// https://dev.to/chuck_huey/setup-a-typescript-project-with-eslint-prettier-editorconfig-and-husky-13aa
-// https://www.robertcooper.me/using-eslint-and-prettier-in-a-typescript-project
